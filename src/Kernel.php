56e638ee3b75e390c486ba5b79040b26b7aeb1db
machekavyj <thetangro@gmail.com>
2022-10-01 20:12:19 +0300

    [7] fix errors

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,29 @@
 
 namespace App;
 
+use Monolog\Logger;
+use PGHandler\PGHandler;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function boot()
+    {
+        parent::boot();
+
+        $this->registerPgLogger();
+    }
+
+    private function registerPgLogger(): void
+    {
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $connection = $em->getConnection()->getNativeConnection();
+        $pgHandler = new PGHandler($connection, 'system_log');
+        $logger = $this->getContainer()->get('logger');
+        /** @var Logger $logger * */
+        $logger->pushHandler($pgHandler);
+    }
 }
